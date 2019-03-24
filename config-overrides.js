3b06b3aec9df527e6c7b7c5f8ea093c608fdf1d0
hawer_star <marklow01@126.com>
2019-03-24 21:22:41 +0800

    home

--- config-overrides.js
+++ config-overrides.js
@@ -1,70 +1,88 @@
-const { injectBabelPlugin } = require('react-app-rewired')
-const WebpackPluginImport = require('webpack-plugin-import')
-const rewireSass = require('./rewire-scss')
-const ProgressBarPlugin = require('progress-bar-webpack-plugin')
-const rewireLess = require('react-app-rewire-less')
-const rewireDefinePlugin = require('react-app-rewire-define-plugin')
-const rewireWebpackBundleAnalyzer = require('react-app-rewire-webpack-bundle-analyzer')
-const path = require('path')
+const { injectBabelPlugin } = require("react-app-rewired");
+const WebpackPluginImport = require("webpack-plugin-import");
+const rewireSass = require("./rewire-scss");
+const ProgressBarPlugin = require("progress-bar-webpack-plugin");
+const rewireLess = require("react-app-rewire-less");
+const rewireDefinePlugin = require("react-app-rewire-define-plugin");
+const rewireWebpackBundleAnalyzer = require("react-app-rewire-webpack-bundle-analyzer");
+const path = require("path");
 
 module.exports = function override(config, env) {
-    config = injectBabelPlugin(
-        ['import', {
-            libraryName: 'antd',
-            libraryDirectory: 'es',
-            style: 'css'
-        }],
-        config)
-    config = injectBabelPlugin(
-        ['import', { libraryName: '@alifd/next' }],
-        config
-    )
-    config.plugins.push(new ProgressBarPlugin())
-    config.plugins.push(
-        new WebpackPluginImport([{
-                libraryName: /^@alifd\/next\/lib\/([^/]+)/,
-                stylePath: 'style.js'
-            },
-            {
-                libraryName: /@icedesign\/.*/,
-                stylePath: 'style.js'
-            }
-        ])
-    )
-    config = injectBabelPlugin(['import', { libraryName: 'antd', style: true }], config)
-    config = rewireLess.withLoaderOptions({
-        modifyVars: { '@primary-color': '#1DA57A' }
-    })(config, env)
-    config = rewireDefinePlugin(config, env, {
-        __DEV__: false
-    })
+  //   config = injectBabelPlugin(
+  //     [
+  //       "import",
+  //       {
+  //         libraryName: "antd",
+  //         libraryDirectory: "es",
+  //         style: "css"
+  //       }
+  //     ],
+  //     config
+  //   );
 
-    config = rewireSass(config)
+  config.plugins.push(new ProgressBarPlugin());
+  config.plugins.push(
+    new WebpackPluginImport([
+      {
+        libraryName: /^@alifd\/next\/lib\/([^/]+)/,
+        stylePath: "style.js"
+      },
+      {
+        libraryName: /@icedesign\/.*/,
+        stylePath: "style.js"
+      }
+    ])
+  );
+  config = injectBabelPlugin(
+    [
+      "import",
+      [
+        { libraryName: "antd", style: true },
+        { libraryName: "@alifd/next", style: true }
+      ]
+    ],
+    config
+  );
+  config = rewireLess.withLoaderOptions({
+    modifyVars: { "@primary-color": "#1DA57A" }
+  })(config, env);
+  //   config = injectBabelPlugin(
+  //     [
+  //       "import",
+  //       { libraryName: "@alifd/next", style: true }
+  //     ],
+  //     config
+  //   );
+  config = rewireDefinePlugin(config, env, {
+    __DEV__: false
+  });
 
-    // 短路径别名配置
-    config.resolve = {
-        ...config.resolve,
-        alias: {
-            // '@': path.resolve(__dirname, 'src'),
-            '@node_modules': path.resolve(__dirname, 'node_modules'),
-            '@api': path.resolve(__dirname, 'src/api'),
-            '@components': path.resolve(__dirname, 'src/components'),
-            '@assets': path.resolve(__dirname, 'src/assets'),
-            '@routerConfig': path.resolve(__dirname, 'src/routerConfig.js'),
-            '@pages': path.resolve(__dirname, 'src/pages'),
-            '@routes': path.resolve(__dirname, 'src/routes'),
-            '@stores': path.resolve(__dirname, 'src/stores'),
-            '@utils': path.resolve(__dirname, 'src/utils'),
+  config = rewireSass(config);
 
-        }
+  // 短路径别名配置
+  config.resolve = {
+    ...config.resolve,
+    alias: {
+      // '@': path.resolve(__dirname, 'src'),
+      "@node_modules": path.resolve(__dirname, "node_modules"),
+      "@api": path.resolve(__dirname, "src/api"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@routerConfig": path.resolve(__dirname, "src/routerConfig.js"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@routes": path.resolve(__dirname, "src/routes"),
+      "@stores": path.resolve(__dirname, "src/stores"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+      "@style": path.resolve(__dirname, "src/style")
     }
+  };
 
-    if (env === 'production') {
-        config = rewireWebpackBundleAnalyzer(config, env, {
-            analyzerMode: 'static',
-            reportFilename: 'report.html'
-        })
-    }
+  if (env === "production") {
+    config = rewireWebpackBundleAnalyzer(config, env, {
+      analyzerMode: "static",
+      reportFilename: "report.html"
+    });
+  }
 
-    return config
-}
+  return config;
+};
--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import React, { Component } from 'react'
-import './style/common.scss'
+import React, { Component } from "react";
+import "./style/common.scss";
 // import logo from './logo.svg'
 // import './App.scss'
 // import Next from '@alifd/next'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from "react-router-dom";
 // 如下方式进行引入，这样就会按需引入所需的js和css了
 // import { Button } from 'antd'
 // import './style.less'
-import routes from '@routerConfig'
-import Banner from '@components/banner'
-import Header from '@components/header'
-import Footer from '@components/footer'
+import routes from "@routerConfig";
+import Banner from "@components/banner";
+import Header from "@components/header";
+import Footer from "@components/footer";
 
 class App extends Component {
   render() {
@@ -25,38 +25,40 @@ class App extends Component {
               <Banner />
               <Router>
                 <Header />
-                {routes.map((route, key) => {
-                  if (route.exact) {
-                    return (
-                      <Route
-                        key={key}
-                        exact
-                        path={route.path}
-                        render={props => (
-                          <route.component {...props} routes={route.routes} />
-                        )}
-                      />
-                    )
-                  } else {
-                    return (
-                      <Route
-                        key={key}
-                        path={route.path}
-                        render={props => (
-                          <route.component {...props} routes={route.routes} />
-                        )}
-                      />
-                    )
-                  }
-                })}
+                <div className="center_borderbox">
+                  {routes.map((route, key) => {
+                    if (route.exact) {
+                      return (
+                        <Route
+                          key={key}
+                          exact
+                          path={route.path}
+                          render={props => (
+                            <route.component {...props} routes={route.routes} />
+                          )}
+                        />
+                      );
+                    } else {
+                      return (
+                        <Route
+                          key={key}
+                          path={route.path}
+                          render={props => (
+                            <route.component {...props} routes={route.routes} />
+                          )}
+                        />
+                      );
+                    }
+                  })}
+                </div>
               </Router>
+              <Footer />
             </div>
           </div>
         </div>
-        <Footer />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
