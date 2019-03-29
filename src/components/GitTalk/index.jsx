908a88544d60d10b88c07b5f0fecdcdfa96d3588
Mrfh <marklow01@126.com>
2019-03-29 16:32:47 +0800

    repo

--- src/components/GitTalk/index.jsx
+++ src/components/GitTalk/index.jsx
@@ -10,7 +10,7 @@ export default class Gitalks extends React.Component {
             enable: true,
             clientID: '636922112cbabb01ff12',
             clientSecret: '0bd30a7dcc73d70e7bb9014200376f5a95268db1',
-            repo: 'hawerblogTalkAbout',
+            repo: 'fh-combo.github.io',//hawerblogTalkAbout
             owner: 'fh-combo',
             admin: 'fh-combo',
             id: window.location.pathname,// this.props.path,
