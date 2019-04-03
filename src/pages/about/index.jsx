4cb6b5e80ab730a9798955394e1ea0d53acf660f
Mrfh <marklow01@126.com>
2019-04-03 18:37:40 +0800

    新增关于我部分样式

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,18 +1,38 @@
-import React, { Component } from 'react'
-class About extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
+import React, { Component } from "react";
+import styles from "./about.module.less";
+import { Row, Card, message, Tag } from "antd";
+const { Meta } = Card;
 
-    }
+class About extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      headImg:"https://avatars2.githubusercontent.com/u/16040462?s=460&v=4"
+    };
   }
-  render () {
+  render() {
     return (
       <div>
-        About组件
+        <div className="article_wrap_block">
+          <div className="left">
+            <div className={styles.head_userbox}>
+              <div className={styles.imagewrap}>
+                <img src={this.state.headImg} alt=""/>
+              </div>
+              <div className={styles.ctrl_bottom}>
+                out sick!
+              </div>
+            </div>
+          </div>
+          <div className="right">
+            <Row>
+
+            </Row>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default About
+export default About;
